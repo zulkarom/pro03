752684b00696746f3db10c5e1732eb57ab17a36c
zulkarom <zulkarom@gmail.com>
2019-08-02 09:52:43 +0800

    update

--- confmanager/views/layouts/main.php
+++ confmanager/views/layouts/main.php
@@ -11,7 +11,7 @@ use common\widgets\Alert;
 use yii\helpers\Url;
 
 confmanager\assets\MainAsset::register($this);
-$directoryAsset = Yii::$app->assetManager->getPublishedUrl('@confmanager/views/myasset');
+$dirAsset = Yii::$app->assetManager->getPublishedUrl('@confmanager/views/myasset');
 
 
 
@@ -33,23 +33,23 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@confmanager/views/m
     <title>Dashboard</title>
 
     <!-- Fontfaces CSS-->
-    <link href="css/font-face.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
-    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="<?=$dirAsset?>/css/font-face.css" rel="stylesheet" media="all">
+    <link href="<?=$dirAsset?>/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="<?=$dirAsset?>/vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="<?=$dirAsset?>/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
 
     <!-- Bootstrap CSS-->
-    <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+    <link href="<?=$dirAsset?>/vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
 
     <!-- Vendor CSS-->
-    <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
-    <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
-    <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
-    <link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="<?=$dirAsset?>/vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+    <link href="<?=$dirAsset?>/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+    <link href="<?=$dirAsset?>/vendor/wow/animate.css" rel="stylesheet" media="all">
+    <link href="<?=$dirAsset?>/vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
 
 
     <!-- Main CSS-->
-    <link href="css/theme.css" rel="stylesheet" media="all">
+    <link href="<?=$dirAsset?>/css/theme.css" rel="stylesheet" media="all">
 
 </head>
 
@@ -61,7 +61,7 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@confmanager/views/m
                 <div class="container-fluid">
                     <div class="header-mobile-inner">
                         <a class="logo" href="index.html">
-                            <img src="images/icon/logo.png" alt="CoolAdmin" />
+                           CONFVALLEY
                         </a>
                         <button class="hamburger hamburger--slider" type="button">
                             <span class="hamburger-box">
@@ -103,7 +103,7 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@confmanager/views/m
         <aside class="menu-sidebar d-none d-lg-block">
             <div class="logo">
                 <a href="#">
-                    <img src="images/icon/logo.png" alt="Cool Admin" />
+                    CONFVALLEY
                 </a>
             </div>
             <div class="menu-sidebar__content js-scrollbar1">
@@ -154,23 +154,23 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@confmanager/views/m
                                 <div class="account-wrap">
                                     <div class="account-item clearfix js-item-menu">
                                         <div class="image">
-                                            <img src="images/icon/avatar-01.jpg" alt="John Doe" />
+                                            <img src="<?=$dirAsset?>/images/icon/avatar.jpg" alt="John Doe" />
                                         </div>
                                         <div class="content">
-                                            <a class="js-acc-btn" href="#">john doe</a>
+                                            <a class="js-acc-btn" href="#">Manager</a>
                                         </div>
                                         <div class="account-dropdown js-dropdown">
                                             <div class="info clearfix">
                                                 <div class="image">
                                                     <a href="#">
-                                                        <img src="images/icon/avatar-01.jpg" alt="John Doe" />
+                                                        <img src="<?=$dirAsset?>/images/icon/avatar.jpg" alt="John Doe" />
                                                     </a>
                                                 </div>
                                                 <div class="content">
                                                     <h5 class="name">
-                                                        <a href="#">john doe</a>
+                                                        <a href="#">Manager</a>
                                                     </h5>
-                                                    <span class="email">johndoe@example.com</span>
+                                                    <span class="email">manager@confvalley.com</span>
                                                 </div>
                                             </div>
                                             <div class="account-dropdown__body">
@@ -227,27 +227,27 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@confmanager/views/m
     </div>
 
     <!-- Jquery JS-->
-    <script src="vendor/jquery-3.2.1.min.js"></script>
+    <script src="<?=$dirAsset?>/vendor/jquery-3.2.1.min.js"></script>
     <!-- Bootstrap JS-->
-    <script src="vendor/bootstrap-4.1/popper.min.js"></script>
-    <script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <script src="<?=$dirAsset?>/vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="<?=$dirAsset?>/vendor/bootstrap-4.1/bootstrap.min.js"></script>
     <!-- Vendor JS       -->
-    <script src="vendor/slick/slick.min.js">
+    <script src="<?=$dirAsset?>/vendor/slick/slick.min.js">
     </script>
-    <script src="vendor/wow/wow.min.js"></script>
-    <script src="vendor/animsition/animsition.min.js"></script>
-    <script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    <script src="<?=$dirAsset?>/vendor/wow/wow.min.js"></script>
+    <script src="<?=$dirAsset?>/vendor/animsition/animsition.min.js"></script>
+    <script src="<?=$dirAsset?>/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
     </script>
-    <script src="vendor/counter-up/jquery.waypoints.min.js"></script>
-    <script src="vendor/counter-up/jquery.counterup.min.js">
+    <script src="<?=$dirAsset?>/vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="<?=$dirAsset?>/vendor/counter-up/jquery.counterup.min.js">
     </script>
-    <script src="vendor/circle-progress/circle-progress.min.js"></script>
-    <script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="<?=$dirAsset?>/vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="<?=$dirAsset?>/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
 
     </script>
 
     <!-- Main JS-->
-    <script src="js/main.js"></script>
+    <script src="<?=$dirAsset?>/js/main.js"></script>
 
 </body>
 
